08773be5a7927976a137b0102d8be5d46f5daa78
Malachai <prussian1933@naver.com>
2023-04-17 22:47:23 +0900

    Feat:Header 수정

--- mata2/src/components/Header.js
+++ mata2/src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from "react-router-dom";
 
 const Header = (props) => {
-
 
 
   const dropdown = () => {
@@ -16,11 +16,13 @@ const Header = (props) => {
           <path
             d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
         </svg>
-        <span className="font-semibold text-xl tracking-tight">MATA</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="font-semibold text-xl text-white tracking-tight">MATA</span>
+        </Link>
       </div>
       <div className="block lg:hidden">
         <button onClick={dropdown}
-          className="flex items-center px-3 py-2 border rounded text-white border-teal-400 hover:text-white hover:border-white">
+                className="flex items-center px-3 py-2 border rounded text-white border-teal-400 hover:text-white hover:border-white">
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
