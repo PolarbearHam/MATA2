2422df912a1e9dda5ddb9e72a67f8f463ec0b6cf
±èȣ¼º <dsrla123@naver.com>
2023-04-17 13:38:50 +0900

    feat welcome컴포넌트 스타일

--- mata2/src/views/Welcome.js
+++ mata2/src/views/Welcome.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import backgroundImage from '../assets/Welcome.jpg'
 
 const Welcome = () => {
+  const styles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    width: '100%',
+    height: '500px',
+  };
+
     return (
-        <div>
-            <div >
-
-            </div>
-
-            <div>
-
-            </div>
+      <div style={styles}>
+        <div className="center-text">
+          <div  >MATA</div>
+          <div>메인입니다.</div>
         </div>
+
+        <div>
+          <div>첫 설명입니다.</div>
+          <div>두 번째 설명입니다.</div>
+        </div>
+      </div>
     );
 };
 
