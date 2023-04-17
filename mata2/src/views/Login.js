eed9003f2105012f8136232741804997aff7c738
±èȣ¼º <dsrla123@naver.com>
2023-04-17 16:29:03 +0900

    feat: 로그인 스타일 수정

--- mata2/src/views/Login.js
+++ mata2/src/views/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
         <div id='loginLogo'>MATA</div>
         <input className='inputField' placeholder='사용자 이름'/>
         <input className='inputField' placeholder='암호'/>
-        <button id='loginButton' className='btn-primary'>로그인</button>
+        <button id='loginButton' >로그인</button>
         
         <div id='finds'>
           <div className='findsText'> 아이디 찾기</div> <div className='findsText'>비밀번호 찾기</div>
