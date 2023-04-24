bc8ab30876a6369be90b945e1f52ad75da57d15b
junho <blacktick@naver.com>
2023-04-24 17:41:46 +0900

    Feat: EventService 서비스 1차 작업 완료

--- spring-api-merged/src/main/java/com/ssafy/DummyData.java
+++ spring-api-merged/src/main/java/com/ssafy/DummyData.java
@@ -1,18 +1,24 @@
 package com.ssafy;
 
-import com.ssafy.entity.Member;
-import com.ssafy.entity.Project;
+import com.ssafy.entity.*;
+import com.ssafy.repository.*;
 import com.ssafy.util.MemberPrivilege;
 import com.ssafy.util.ProjectCategory;
-import com.ssafy.repository.MemberRepository;
-import com.ssafy.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.mortbay.util.ajax.JSON;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 
 @Component
@@ -20,14 +26,48 @@ import java.util.List;
 public class DummyData implements CommandLineRunner {
 
     private final MemberRepository memberRepository;
-
     private final ProjectRepository projectRepository;
+    private final EventRepository eventRepository;
+    private final EventParamRepository eventParamRepository;
+    private final EventPathRepository eventPathRepository;
+    private final TagRepository tagRepository;
+    private final TagEventRepository tagEventRepository;
     private final PasswordEncoder passwordEncoder;
+
 
     @Override
     public void run(String... args) throws Exception {
         addMember();
         addProject();
+        addEvent();
+    }
+
+    private void addEvent() throws IOException {
+        String stringDummy = "{\"events\": { \"login\": { \"base\": \"click\", \"param\": [], \"path\": [ {\"name\": \"userId\", \"index\": 2} ] }, \"purchase\": { \"base\": \"click\", \"param\": [ {\"name\": \"productName\", \"key\": \"product\"}, {\"name\": \"productName2\", \"key\": \"product2\"} ], \"path\": [ {\"name\": \"productId\", \"index\": 3} ] } }}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(stringDummy);
+        for (Iterator<Map.Entry<String, JsonNode>> it = jsonNode.get("events").getFields(); it.hasNext(); ) {
+            Map.Entry<String, JsonNode> event = it.next();
+            Event temp = Event.builder()
+                    .eventName(event.getKey())
+                    .eventBase(event.getValue().get("base").toString())
+                    .build();
+            eventRepository.save(temp);
+            for (JsonNode param : event.getValue().get("param")) {
+                eventParamRepository.save(EventParam.builder()
+                        .event(temp)
+                        .paramKey(param.get("key").toString())
+                        .paramName(param.get("name").toString())
+                        .build());
+            }
+            for (JsonNode param : event.getValue().get("path")) {
+                eventPathRepository.save(EventPath.builder()
+                        .event(temp)
+                        .pathIndex(param.get("index").toString())
+                        .pathName(param.get("name").toString())
+                        .build());
+            }
+        }
     }
 
     private void addProject() {
