bbf1e00915c0ed5d61f249406a8b7f3e99334586
junho <blacktick@naver.com>
2023-04-24 15:30:20 +0900

    Feat: 더미 데이터 생성을 위한 레퍼지토리 생성

--- spring-api-merged/src/main/java/com/ssafy/repository/EventRepository.java
+++ spring-api-merged/src/main/java/com/ssafy/repository/EventRepository.java
@@ -1,11 +1,13 @@
 package com.ssafy.repository;
 
+import com.ssafy.entity.Event;
 import com.ssafy.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Optional<Project> findByToken(String token);
+public interface EventRepository extends JpaRepository<Event, Long> {
+//    Optional<Event> findById(long eventId);
 }
--- spring-api-merged/src/main/java/com/ssafy/repository/TagEventRepository.java
+++ spring-api-merged/src/main/java/com/ssafy/repository/TagEventRepository.java
@@ -1,11 +1,14 @@
 package com.ssafy.repository;
 
 import com.ssafy.entity.Project;
+import com.ssafy.entity.TagEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Optional<Project> findByToken(String token);
+public interface TagEventRepository extends JpaRepository<TagEvent, Long> {
+    List<TagEvent> findAllByTagId(long tagId);
 }
