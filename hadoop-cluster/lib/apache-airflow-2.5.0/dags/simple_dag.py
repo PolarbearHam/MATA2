d3c28ebb618f6d876c057ae80545692406c1dbd5
Malachai <prussian1933@naver.com>
2023-05-10 17:47:03 +0900

    Feat: npm-mata 업데이트, 테스트용 DAG 추가

---
+++ hadoop-cluster/lib/apache-airflow-2.5.0/dags/simple_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.apache.hive.operators.hive import HiveOperator
+from spark import Batching_Jobs
+
+with DAG(
+        dag_id="simple_workflow_dag",
+        schedule=None,
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        tags=["test"],
+) as dag:
+    base_date = pendulum.date(2013, 1, 1)
+    iteration = 5
+
+    @task(task_id="first_job")
+    def first(date: str) -> str:
+        print("this is the first step :" + date)
+        return date
+
+    @task(task_id="second_job")
+    def second(src: str):
+        print("this is the second step :" + src)
+        third()
+
+    @task(task_id="third_job")
+    def third():
+        print("this is the third step :")
