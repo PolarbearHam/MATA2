14473a183513b620c3e1f5432931fb1f4ee99a1f
wlsdnr4729 <wlsdnr4729@naver.com>
2023-05-11 11:16:32 +0900

    feat: web title 수정

--- mata2/src/dashboards/DashboardMain.js
+++ mata2/src/dashboards/DashboardMain.js
@@ -295,19 +295,19 @@ class ToolboxLayout extends React.Component {
   }
 }
 
-function generateLayout() {
-  return _.map(_.range(0, 25), function(item, i) {
-    var y = Math.ceil(Math.random() * 4) + 1;
-    return {
-      x: (_.random(0, 5) * 2) % 12,
-      y: Math.floor(i / 6) * y,
-      w: 2,
-      h: y,
-      i: i.toString(),
-      static: Math.random() < 0.05
-    };
-  });
-}
+// function generateLayout() {
+//   return _.map(_.range(0, 25), function(item, i) {
+//     var y = Math.ceil(Math.random() * 4) + 1;
+//     return {
+//       x: (_.random(0, 5) * 2) % 12,
+//       y: Math.floor(i / 6) * y,
+//       w: 2,
+//       h: y,
+//       i: i.toString(),
+//       static: Math.random() < 0.05
+//     };
+//   });
+// }
 const ANALYTICS_URL = "http://localhost:8080/api/v1/analytics";
 const REQUEST_STANDARD_TIME = Date.now();
 
